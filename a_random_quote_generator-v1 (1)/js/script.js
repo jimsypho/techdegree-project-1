fd5bd89a53e2ea97e072053ad20c0879c6d413a9
jimsypho <jpsypho@gmail.com>
2020-01-27 21:32:50 -0600

    Added quotes array of objects

--- a_random_quote_generator-v1 (1)/js/script.js
+++ a_random_quote_generator-v1 (1)/js/script.js
@@ -10,7 +10,30 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+var quotes = [
+  {
+    quote : "People don't buy until you move them emotionally.  Thing of all of the times people didn't buy into what you had to offer even though it was the logical thing to do.",
+    source : "Steve Brown",
+    year: 2019
+  },
+  {
+    quote : "Knowing is not enough, we must apply. Willing is not enough, we must do.",
+    source : "Bruce Lee",
+    citation: "brainyquote.com"
+  },
+  {
+    quote : "Mistakes are always forgivable, if one has the courage to admit them.",
+    source : "Bruce Lee"
+  },
+  {
+    quote : "A wise man can learn more from a foolish question than a fool can learn from a wise answer.",
+    source : "Bruce Lee"
+  },
+  {
+    quote : "Never doubt that a small group of thoughtful, committed citizens can change the world; indeed, it's the only thing that ever has.",
+    source : "Margaret Mead"
+  }
+]
 
 
 /***
